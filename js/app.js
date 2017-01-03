7bc6125c37c0572538529d2d8a2cbbef915c73fb
Edbert Voo <developer@edbertvoo.com>
2017-01-02 19:36:42 -0700

    Animated Scroll to Top

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+/**
+ * Initialize foundation
+ */
 $(document).foundation();
+
+
+$(document).ready(function(){
+    $('#scrollToTop').on('click', function(){
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+});
