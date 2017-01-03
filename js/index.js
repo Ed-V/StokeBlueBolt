3c2f07238e1f32ac981ce490a0a5b2ceab3d2a01
Edbert Voo <developer@edbertvoo.com>
2017-01-02 19:36:28 -0700

    FitText Initialization

---
+++ js/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by edbertvoo on 2017-01-02.
+ */
+$(".responsive_headline").fitText(1.2, { minFontSize: '8px', maxFontSize: '40px' });
+$(".responsive_block").fitText(1.2, { minFontSize: '8px', maxFontSize: '35px' });
